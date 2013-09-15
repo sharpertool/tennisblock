410d33769b46f868df65a27d0f621b0422964a1c
Ed Henderson <ed@eeweb.com>
2013-09-15 17:20:04 -0600

    Trying to make a 'Django version' of the PickTeams... This won't be too bad, as I can re-use most all of it, just need to update the DBTeams.py part to use the Django database.. so that's pretty cool,

---
+++ tennisblock_djroot/scripts/TennisBlock/DBTeams.py
@@ -0,0 +1,137 @@
+
+import os
+import datetime
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tennisblock_dj.settings.dev'
+
+from django.db import connection
+from blockdb.models import *
+
+from Player import *
+
+class DBTeams(object):
+
+    def __init__(self,matchid=None):
+
+        #self.sid = self.getSeasonId()
+        if matchid is not None:
+            self.matchid = matchid
+        else:
+            self.matchid = self.getNextMatch()
+
+    def currentSeason(self):
+        seasons = Season.objects.filter(enddate__gte = datetime.date.today())
+        if len(seasons) > 0:
+            return seasons[0]
+
+        return None
+
+    def nextMeeting(self,season):
+        meetings = Meetings.objects \
+            .order_by('date') \
+            .filter(season=season, date__gte = datetime.date.today())
+
+        mtg = None
+        if len(meetings) > 0:
+            mtg = meetings[0]
+
+        return mtg
+
+    def getMatchid(self):
+        return self.matchid
+
+    def getPlayerID(self,name):
+
+        f,l = name.split()
+        try:
+            p = Player.objects.get(first=f,last=l)
+
+            return p
+
+        except:
+            return None
+
+    def getNextMatch(self):
+
+        s = self.currentSeason()
+        m = self.nextMeeting(s)
+
+        return m
+
+    def getPlayers(self,filter=None):
+
+        m = self.getNextMatch()
+        men = []
+        women = []
+
+        return men,women
+
+    def initTeamGen(self):
+        #Slots.objects.filter(match = self.matchid).delete()
+        pass
+
+    def InsertRecords(self,seq):
+        """
+        Insert the sequence
+        """
+
+        self.initTeamGen()
+        m = self.getNextMatch()
+
+        # Insert the slots
+
+        set = 1
+        for s in seq:
+            court = 1
+            for m in s.matches:
+
+                m1 = m.t1.m
+                f1 = m.t1.f
+                m2 = m.t2.m
+                f2 = m.t2.f
+
+                positions = {
+                    'tapa'  : m1.pid,
+                    'tapb'  : f1.pid,
+                    'tbpa'  : m2.pid,
+                    'tbpb'  : f2.pid
+                }
+
+                for position in positions.keys():
+                    pid = positions[position]
+                    sql = """
+                        insert into slots
+                            (matchid, setnum, court,pid,position,combokey)
+                        values (
+                            %d,
+                            %d,
+                            %d,
+                            %d,
+                            "%s",
+                            ""
+                        )
+                            """ % (self.matchid,set,court,pid,position)
+
+                    """
+                    s = Slots.objects.create(
+                        match = m,
+                        set = set,
+                        court = court,
+                        player = pid,
+                        position = position
+                    )
+                    s.save()
+                    """
+
+                court = court + 1
+
+            set = set + 1
+
+def main():
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ tennisblock_djroot/scripts/TennisBlock/TeamGen2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from Player import *
+from Team import *
+from Match import *
+from Set import *
+from Meeting import *
+
+import random
+
+class TeamGen:
+
+    def __init__(self,courts,nSeqs,men,women):
+        self.nCourts = courts
+        self.meeting = Meeting(courts,nSeqs,men,women)
+        self.diffMax = 0.1
+        self.MaxBadDiff = 1.0
+        self.nSequences = nSeqs
+        self.iterLimit = 1000
+
+    def GenerateSetSequences(self,dups):
+        retries = 0
+        self.meeting.Restart()
+        diffMax = 0.1
+
+        self.meeting.ms.setSeeGirlsOnce(dups)
+
+        while (self.meeting.SetCount() < self.nSequences):
+            set = None
+
+            while diffMax <= 2.8 and set == None:
+                set = self.meeting.GetNewSet(diffMax)
+
+
+                if set == None:
+                    diffMax = self.meeting.DiffHistoryMin()
+                    #diffMax = diffMax + 0.1
+                    print "DiffMax Increased to %5.3f" % diffMax
+
+
+            if set == None:
+                self.meeting.PrintCheckStats()
+                print "Failed to build the sequence."
+                return None
+            else:
+                set.Display()
+                dMax,dAvg, DiffList = set.DiffStats()
+                diffs = ",".join(["%5.3f" % x for x in DiffList])
+                print "Found a set sequence with DiffMax:%5.3f Max:%3.3f Avg:%5.3f List:%s" % (self.diffMax,dMax,dAvg,diffs)
+                self.meeting.AddSet(set)
+
+        return self.meeting.sets
+
+    def DisplaySequences(self,seq):
+        for s in seq:
+            s.Display()
+
+
+    def showAllDiffs(self,seq):
+        [s.showDiffs() for s in seq]
+
+
