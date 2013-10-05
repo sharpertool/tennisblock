60de52af8ddd8482ec38b292b94a5ca8746ab03c
Ed Henderson <ed@eeweb.com>
2013-10-05 17:26:18 -0600

    Starting to do a little bit of styling..

--- tennisblock_djroot/static/js/LessPrefix.js
+++ tennisblock_djroot/static/js/LessPrefix.js
@@ -10,7 +10,7 @@ less = {
     async: false,       // load imports async
     fileAsync: false,   // load imports async when in a page under
     // a file protocol
-    poll: 20000,         // when in watch mode, time in ms between polls
+    poll: 2000,         // when in watch mode, time in ms between polls
     functions: {},      // user functions, keyed by name
     dumpLineNumbers: "comments", // or "mediaQuery" or "all"
     relativeUrls: false// whether to adjust url's to be relative
