74909baf71f05ed340a6c78ecdd254c16359c9a3
Ed Henderson <ed@eeweb.com>
2014-01-04 18:17:45 -0700

    Finish my dev builds.. it actually worked.

--- tennisblock/settings/dev.py
+++ tennisblock/settings/dev.py
@@ -10,9 +10,9 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tennisblock',                      # Or path to database file if using sqlite3.
+        'NAME': 'tennisblock_prod_mirror',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'tennisblock_prod_mirror',
+        'USER': 'tennisblock',
         'PASSWORD': 'P5HJTdHt5dR2t9Q2',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
--- tennisblock/urls.py
+++ tennisblock/urls.py
@@ -4,26 +4,23 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from tennisblock.views import HomeView
+from views import HomeView, BlockSchedule, AvailabilityView
 from tennisblock.about.views import AboutView
 from tennisblock.contact.views import ContactView
-from tennisblock.availability.views import AvailabilityView
 from tennisblock.playsheet.views import PlaysheetView
 
 urlpatterns = patterns('',
                # Examples:
                url(r'^$', HomeView.as_view()),
-               url(r'^api/', include('tennisblock.api.urls')),
-               url(r'^schedule/', include('tennisblock.schedule.urls')),
+               url(r'^schedule/', BlockSchedule.as_view()),
                url(r'^availability/', AvailabilityView.as_view()),
+
+               url(r'^api/', include('tennisblock.api.urls')),
                url(r'^playsheet/', PlaysheetView.as_view()),
                url(r'^members/', include('tennisblock.members.urls')),
                url(r'^contact/?', ContactView.as_view()),
                url(r'^about/?', AboutView.as_view()),
                url(r'^accounts/', include('tennisblock.accounts.urls')),
-
-               # Uncomment the admin/doc line below to enable admin documentation:
-               #url(r'^kag3hewb/doc/', include('django.contrib.admindocs.urls')),
 
                # Uncomment the next line to enable the admin:
                url(r'^kag3hewb/', include(admin.site.urls)),
