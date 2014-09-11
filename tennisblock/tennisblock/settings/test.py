9afd97c7c21fd1946e94fea36e905d6380a9fa1f
Ed Henderson <ed@eeweb.com>
2014-09-11 13:16:19 -0600

    Added Sentry

--- tennisblock/tennisblock/settings/test.py
+++ tennisblock/tennisblock/settings/test.py
@@ -5,6 +5,11 @@ from .base import *
 
 DATABASES['default']['NAME'] = 'tennisblock_test'
 
+# Set your DSN value
+RAVEN_CONFIG = {
+    'dsn': 'https://d5b96362c1574d68bf050ecfd8d4a2f4:8024bcdfe7b143acab9be3ad4ba38118@app.getsentry.com/29740',
+    }
+
 try:
     from local_settings import *
 except ImportError:
