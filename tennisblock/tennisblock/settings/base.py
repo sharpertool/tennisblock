5d19e75c38015d2e2e770314f88a6116442eab71
Ed Henderson <ed@eeweb.com>
2015-01-16 20:03:29 -0700

    Another big change. Moved the static folder into webapp, and removed the explicit static path.

--- tennisblock/tennisblock/settings/base.py
+++ tennisblock/tennisblock/settings/base.py
@@ -101,14 +101,6 @@ STATIC_ROOT = PROJECT_ROOT.child('collectedstatic')
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    DJANGO_ROOT.child('static'),
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
