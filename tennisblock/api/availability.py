ea90b0866ee950a0d77390cf09f6b3984a81779f
Ed Henderson <ed@eeweb.com>
2015-12-30 17:37:41 -0700

    Reformatting only.

--- tennisblock/api/availability.py
+++ tennisblock/api/availability.py
@@ -1,35 +1,34 @@
 import datetime
 from django.views.generic.base import View
 from rest_framework.parsers import JSONParser
-from blockdb.models import Player,SeasonPlayers,Meetings,Availability,Schedule
+from blockdb.models import Player, SeasonPlayers, Meetings, Availability, Schedule
 
 from .apiutils import JSONResponse, get_current_season, get_meeting_for_date
 
 
-def _AvailabilityInit(player,meetings):
+def _AvailabilityInit(player, meetings):
     """
     Add blank availability items for the specified player
     """
 
     for mtg in meetings:
-        av = Availability.objects.filter(meeting=mtg,player=player)
+        av = Availability.objects.filter(meeting=mtg, player=player)
 
-        if len(av)== 0:
+        if len(av) == 0:
             av = Availability.objects.create(
-                meeting=mtg,
-                player=player,
-                available = True
+                    meeting=mtg,
+                    player=player,
+                    available=True
             )
             av.save()
 
 
 class AvailabilityView(View):
-
-    def get(self,request):
+    def get(self, request):
 
         currseason = get_current_season()
-        mtgs = Meetings.objects.filter(season = currseason).order_by('date')
-        players = SeasonPlayers.objects.filter(season = currseason)
+        mtgs = Meetings.objects.filter(season=currseason).order_by('date')
+        players = SeasonPlayers.objects.filter(season=currseason)
 
         past_mtgs = Meetings.objects.filter(season=currseason, date__lte=datetime.date.today())
         future_mtgs = Meetings.objects.filter(season=currseason, date__gt=datetime.date.today())
@@ -50,17 +49,17 @@ class AvailabilityView(View):
             nscheduled = Schedule.objects.filter(meeting__in=future_mtgs, player=player).count()
 
             p = {
-                'name' : player.first + ' ' + player.last,
-                'id' : player.id,
-                'isavail' : avail,
-                'scheduled' : scheduled,
-                'nplayed' : nplayed,
+                'name': player.first + ' ' + player.last,
+                'id': player.id,
+                'isavail': avail,
+                'scheduled': scheduled,
+                'nplayed': nplayed,
                 'nscheduled': nscheduled + nplayed
             }
 
             avlist = Availability.objects.filter(player=player, meeting__in=mtgs).order_by('meeting__date')
             if avlist.count() == 0:
-                _AvailabilityInit(player,mtgs)
+                _AvailabilityInit(player, mtgs)
                 avlist = Availability.objects.filter(player=player, meeting__in=mtgs).order_by('meeting__date')
 
             sched = Schedule.objects.filter(player=player, meeting__in=future_mtgs).order_by('meeting__date')
@@ -72,29 +71,27 @@ class AvailabilityView(View):
                 sch = Schedule.objects.filter(player=player, meeting=mtg)
                 scheduled.append(sch.count() > 0)
 
-
             pdata.append(p)
 
         return JSONResponse(pdata)
 
-    def put(self,request):
-    #elif request.method == 'PUT':
+    def put(self, request):
+        # elif request.method == 'PUT':
 
         data = JSONParser().parse(request)
         currseason = get_current_season()
-        mtgs = Meetings.objects.filter(season = currseason).order_by('date')
+        mtgs = Meetings.objects.filter(season=currseason).order_by('date')
         try:
             mtg = mtgs[data['mtgidx']]
             p = Player.objects.get(pk=data['id'])
-            print("Updating availability for %s on %s to %s" % (p.Name(),mtg.date,data['isavail']))
-            av = Availability.objects.get(meeting=mtg,player=p)
+            print("Updating availability for %s on %s to %s" % (p.Name(), mtg.date, data['isavail']))
+            av = Availability.objects.get(meeting=mtg, player=p)
             av.available = data['isavail']
             av.save()
         except:
             print("Error trying to update availability")
 
-        #data = request.DATA
+            # data = request.DATA
         # Update availability for someone.
 
         return JSONResponse({})
-
--- tennisblock/webapp/urls.py
+++ tennisblock/webapp/urls.py
@@ -14,27 +14,24 @@ from .views import (
 )
 
 urlpatterns = (
-               # Examples:
-               url(r'^$', HomeView.as_view(), name='home'),
+    # Examples:
+    url(r'^$', HomeView.as_view(), name='home'),
 
-               url(r'^availability/', AvailabilityView.as_view(),
-                   name='availability'),
-               url(r'^availability_form/$', AvailabilityFormView.as_view(),
-                   name='availability_form'),
-               url(r'^availability_form/(?P<pk>\d+)/?$', AvailabilityFormView.as_view(),
-                   name='availability_form_post'),
+    url(r'^availability/', AvailabilityView.as_view(), name='availability'),
+    url(r'^availability_form/$', AvailabilityFormView.as_view(), name='availability_form'),
+    url(r'^availability_form/(?P<pk>\d+)/?$', AvailabilityFormView.as_view(), name='availability_form_post'),
 
-               url(r'^playsheet/', PlaysheetView.as_view()),
-               url(r'^accounts/', include('accounts.urls')),
-               url(r'^couples/(?P<pk>\d+)/$', CouplesView.as_view(), name="couple_editor"),
-               url(r'^contact/?', ContactView.as_view(), name="contact"),
-               url(r'^about/?', AboutView.as_view(), name="about"),
+    url(r'^playsheet/', PlaysheetView.as_view()),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^couples/(?P<pk>\d+)/$', CouplesView.as_view(), name="couple_editor"),
+    url(r'^contact/?', ContactView.as_view(), name="contact"),
+    url(r'^about/?', AboutView.as_view(), name="about"),
 
-               url(r'^season/', include('season.urls')),
-               url(r'^members/', include('members.urls')),
-               url(r'^schedule/', include('schedule.urls')),
-               url(r'^api/', include('api.urls')),
+    url(r'^season/', include('season.urls')),
+    url(r'^members/', include('members.urls')),
+    url(r'^schedule/', include('schedule.urls')),
+    url(r'^api/', include('api.urls')),
 
-               # Uncomment the next line to enable the admin:
-               url(r'^kag3hewb/', include(admin.site.urls)),
-       )
+    # Uncomment the next line to enable the admin:
+    url(r'^kag3hewb/', include(admin.site.urls)),
+)
