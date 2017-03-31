73479db85b7a76ca56f90007f9ca2afaae4d362d
Ed Henderson <ed@eeweb.com>
2017-03-31 14:10:39 -0600

    Remove south. Add management command to pick teams.

---
+++ tennisblock/api/management/commands/pick_teams.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+
+from django.core.management.base import BaseCommand, CommandError
+from TBLib.teams import TeamManager
+
+class Command(BaseCommand):
+    help = 'Pick Teams for the given date'
+
+    def add_arguments(self, parser):
+        parser.add_argument('date')
+        parser.add_argument('--nodupes', action='store_true')
+        parser.add_argument('--sequences', type=int, default=3)
+        parser.add_argument('--test', '-t', action='store_true')
+
+    def handle(self, *args, **options):
+
+        mgr = TeamManager()
+        mgr.pickTeams(options['date'],
+                      test=options['test'],
+                      sequences=options['sequences'],
+                      nodupes=options['nodupes'])
