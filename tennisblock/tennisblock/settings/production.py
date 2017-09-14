b489ef5fd74354eeb909805616cc2f4ca9f36160
Ed Henderson <ed@sharpertool.com>
2017-09-14 10:45:41 -0600

    Update production email config

--- tennisblock/tennisblock/settings/production.py
+++ tennisblock/tennisblock/settings/production.py
@@ -21,6 +21,10 @@ RAVEN_CONFIG = {
     'dsn': 'https://bb218b1fa4274266aea0a33a4a10c0a5:9772e132d1904c99909fc13e2fc16da7@app.getsentry.com/24185',
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'secure.emailsrvr.com'
+EMAIL_PORT = 465
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
