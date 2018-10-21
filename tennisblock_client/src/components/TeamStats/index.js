baccb2864da51ca5280df40c1d11ac09e6d46e93
ed@sharpertool.com <ed@sharpertool.com>
2018-10-20 20:20:05 -0600

    Add bar to represent spread

--- tennisblock_client/src/components/TeamStats/index.js
+++ tennisblock_client/src/components/TeamStats/index.js
@@ -8,27 +8,33 @@ const teamstats = (props) => {
   const {teams} = props
   const spread = teams.map(t => {
     const {f: {untrp: fu}, m: {untrp: fm}} = t
-    return Math.abs(fu - fm).toFixed(1)
+    return Math.abs(fu - fm)
   })
 
   const mainClass = ['row', classes.TeamStats].join(' ')
+
+  const barWidth = Math.min(100, 100*(spread[0]+spread[1])/2)
 
 
   return (
     <div className="col-12">
       <div className={mainClass}>
         <div className="col-6">
-          {spread[0]}
+          {spread[0].toFixed(1)}
         </div>
         <div className="col-6">
-          {spread[1]}
+          {spread[1].toFixed(1)}
         </div>
       </div>
       <div className={mainClass}>
         <div className="col">
-          <svg width="250" height="30">
+          <svg width="100%" height="30">
             <g>
-              <rect width="250" height="30"/>
+              <rect className={classes.outer} width="100%" height="30"/>
+              <rect className={classes.gauge}
+                  transform={`translate(1,1)`}
+                    width={barWidth+'%'}
+                    height="28"/>
             </g>
           </svg>
         </div>
