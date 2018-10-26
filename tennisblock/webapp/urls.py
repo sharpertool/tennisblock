9fd8d9ec09004f35d8d39aa226df8cd28af40ce4
ed@sharpertool.com <ed@sharpertool.com>
2018-10-26 11:34:51 -0600

    Resolve some django 2.1.x version updates issues. Update some of the url files to modern approach

--- tennisblock/webapp/urls.py
+++ tennisblock/webapp/urls.py
@@ -1,6 +1,4 @@
-from django.urls import include, re_path
-
-# Uncomment the next two lines to enable the admin:
+from django.urls import include, path
 from django.contrib import admin
 
 from .views import (
@@ -15,18 +13,18 @@ from .views import (
 
 urlpatterns = (
     # Examples:
-    re_path(r'^$', HomeView.as_view(), name='home'),
-    re_path(r'^availability/', AvailabilityView.as_view(), name='availability'),
-    re_path(r'^availability_form/$', AvailabilityFormView.as_view(), name='availability_form'),
-    re_path(r'^availability_form/(?P<pk>\d+)/?$', AvailabilityFormView.as_view(), name='availability_form_post'),
-    re_path(r'^playsheet/', PlaysheetView.as_view()),
-    re_path(r'^accounts/', include('accounts.urls')),
-    re_path(r'^couples/(?P<pk>\d+)/$', CouplesView.as_view(), name="couple_editor"),
-    re_path(r'^contact/?', ContactView.as_view(), name="contact"),
-    re_path(r'^about/?', AboutView.as_view(), name="about"),
-    re_path(r'^season/', include('season.urls')),
-    re_path(r'^members/', include('members.urls')),
-    re_path(r'^schedule/', include('schedule.urls')),
-    re_path(r'^api/', include('api.urls')),
-    re_path(r'^django-admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('availability/', AvailabilityView.as_view(), name='availability'),
+    path('availability_form/', AvailabilityFormView.as_view(), name='availability_form'),
+    path('availability_form/<int:pk>/', AvailabilityFormView.as_view(), name='availability_form_post'),
+    path('playsheet/', PlaysheetView.as_view()),
+    path('accounts/', include('accounts.urls')),
+    path('couples/<int:pk>>', CouplesView.as_view(), name="couple_editor"),
+    path('contact/', ContactView.as_view(), name="contact"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('season/', include('season.urls')),
+    path('members/', include('members.urls')),
+    path('schedule/', include('schedule.urls')),
+    path('api/', include('api.urls')),
+    path('django-admin/', admin.site.urls),
 )
