a3f5a62c98426ca02732e43bf8ae21b2eeb2f3e2
jdltechworks <jdltechworks@gmail.com>
2018-11-19 16:48:03 +0800

    Adding new route blockplayers api request plus quick react component

--- tennisblock_client/src/Schedule/modules/teams/actions.js
+++ tennisblock_client/src/Schedule/modules/teams/actions.js
@@ -6,3 +6,16 @@ export const updatePlaySchedule = (schedule) => ({
   type: types.UPDATE_PLAY_SCHEDULE,
   payload: schedule
 })
+
+
+export const getBlockPlayers = (id) => {
+  return {
+    type: types.GET_BLOCK_PLAYERS,
+    id
+  }
+}
+
+export const setBlockPlayers = (blockplayers) => {
+  console.log(types.BLOCK_PLAYERS_FETCHED)
+  return { type: types.BLOCK_PLAYERS_FETCHED, blockplayers }
+}
