4aa4344415d1cd1a2e26f1d040152a695ec5332a
jdltechworks <jdltechworks@gmail.com>
2018-11-22 13:37:17 +0800

    matchdata api call

--- tennisblock_client/src/Schedule/modules/teams/sagas/index.js
+++ tennisblock_client/src/Schedule/modules/teams/sagas/index.js
@@ -4,6 +4,15 @@ import axios from '~/axios-tennisblock'
 import * as actions from '~/Schedule/modules/teams/actions'
 import * as types from '~/Schedule/modules/teams/constants' 
 
+function* requestMatchData() {
+  const date = '2018-09-21'
+  const response = yield call(axios.get, '/api/matchdata')
+  console.log(response)
+  yield put(actions.updatePlaySchedule(response.data))
+  console.log(response.data)
+}
+
+
 function* requestBlockPlayers(action) {
   try {
     const { data } = yield call(axios.get, `/api/blockplayers/${action.id}`)
