42e2fc8289385704ab786026bbe780fad37abf7a
Kin <freepeace13@gmail.com>
2018-11-26 15:11:02 +0800

    Create wagtail about us page

---
+++ tennisblock/about/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+class About(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+    class Meta:
+        verbose_name = "About Page"
