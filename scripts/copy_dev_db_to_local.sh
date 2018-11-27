e669560c1145e9220b8da33b03c8d2b19dd96f1f
ed@sharpertool.com <ed@sharpertool.com>
2018-11-27 15:09:29 -0700

    Update to snapshot script to work with docker environment variables

--- scripts/copy_dev_db_to_local.sh
+++ scripts/copy_dev_db_to_local.sh
@@ -3,11 +3,16 @@
 server=localhost
 dt=$(date '+%Y-%m-%d-%H_%M')
 filename="tennisblock_demo_db_${dt}.sql"
+set -o allexport
+source .env
+set +o allexport
+
 backup_path=$(cat docker-compose.yml \
     | yq -r '.services.postgres.volumes[] | select(test("/backups$")) | split(":")[0]')
 #backup_path=~/Dropbox/GardenbuzzDev/Development/database/backups
+echo "Extracted backup path as ${backup_path} from docker-compose.yml"
 eval full_filename=${backup_path}/${filename}
 echo "Dumping to filename ${full_filename}"
 
-ssh tb "pg_dump -h ${server} -U tennisblock tennisblock"  > ${full_filename}
+ssh tb "pg_dump -h ${server} -U tennisblock tennisblock"  > "${full_filename}"
 
