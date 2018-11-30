a90f9cc0ab695b7b84773e3f4db9dc1c1dff547c
ed@sharpertool.com <ed@sharpertool.com>
2018-11-29 20:53:36 -0700

    Fix bug in available api call. Fix issue with availability styling

--- tennisblock/webapp/static/js/angular/modules/tennisblock-mod.js
+++ tennisblock/webapp/static/js/angular/modules/tennisblock-mod.js
@@ -1,206 +1,207 @@
 angular.module('tennisblock', [])
-    //.config()
-    .controller('BlockSchedule', function($scope,$http) {
-        $scope.dates = [];
-        $scope.guys = [];
-        $scope.gals = [];
-        $scope.subs = [];
-        $scope.queryDate = null;
-        $scope.initialized = false;
-
-        var updateInitialized = function() {
-            if ($scope.dates.length > 0
-                && $scope.guys.length > 0
-                && $scope.gals.length > 0) {
-                $scope.initialized = true;
-            } else {
-                $scope.initialized = false;
-            }
-
-            $scope.isLastDate = isLastBlockDate();
-            $scope.isFirstDate = isFirstBlockDate();
-        };
-
-        $http.get('/api/blockdates').success(function(data) {
-            $scope.dates= data;
-            $scope.firstDate = tb.utils.pyDate2js(data[0].date);
-            $scope.lastDate = tb.utils.pyDate2js(data[data.length-1].date);
-            $scope.blocksched = {};
-            _.each(data,function(d) {
-                var bdate = tb.utils.pyDate2js(d.date);
-                var ho = d.holdout;
-                $scope.blocksched[bdate] = ho;
-            });
-            updateInitialized();
-        });
-
-        var updateAll = function() {
-            $scope.initialized = false;
-
-            $http({
-                'url'       :'/api/blockplayers/date}/',
-                'method'    : 'GET',
-                'params'    : {'date' : $scope.queryDate}
-            }).success(function(data) {
-                    $scope.currdate = tb.utils.pyDate2js(data.date).toLocaleDateString();
-                    $scope.guys = data.guys;
-                    $scope.gals = data.gals;
-                    updateInitialized();
-                });
-
-            $http({
-                'url'       :'/api/subs/',
-                'method'    : 'GET',
-                'params'    : {'date' : $scope.queryDate}
-            }).success(function(data) {
-                    $scope.subs = data;
-                    updateInitialized();
-                });
-
-            $http({
-                'url'       :'/api/blockschedule/',
-                'method'    : 'GET',
-                'params'    : {'date' : $scope.queryDate}
-            }).success(function(data) {
-                    $scope.slots= data;
-                    updateInitialized();
-                });
-        };
-
-        var isLastBlockDate = function() {
-            if ($scope.lastDate && $scope.currdate) {
-                return $scope.currdate == $scope.lastDate.toLocaleDateString();
-            }
-
-            return false;
-        };
-
-        var isFirstBlockDate = function() {
-            if ($scope.firstDate && $scope.currdate) {
-                return $scope.currdate == $scope.firstDate.toLocaleDateString();
-            }
-            return false;
-        };
-
-        var previousBlockDate = function() {
-            var d1 = new Date($scope.currdate);
-            if (d1.toDateString() == $scope.firstDate.toDateString()) {
-                return d1;
-            }
-            var d2 = new Date(d1.getFullYear(), d1.getMonth(),d1.getDate()-7);
-            while (d2.toDateString() != $scope.lastDate.toDateString() && $scope.blocksched[d2] == 1) {
-                d2 = new Date(d2.getFullYear(), d2.getMonth(),d2.getDate()-7);
-            }
-
-            return d2;
-        };
-
-        var nextBlockDate = function() {
-            var d1 = new Date($scope.currdate);
-            if (d1.toDateString() == $scope.lastDate.toDateString()) {
-                return d1;
-            }
-            var d2 = new Date(d1.getFullYear(), d1.getMonth(),d1.getDate()+7);
-            while (d2.toDateString() != $scope.lastDate.toDateString() && $scope.blocksched[d2] == 1) {
-                d2 = new Date(d2.getFullYear(), d2.getMonth(),d2.getDate()+7);
-            }
-
-            return d2;
-        };
-
-        /**
-         * schedulePlayer
-         *
-         * Update the schedule for the current block session
-         */
-        $scope.schedulePlayer = function() {
-
-        };
-
-        $scope.isHoldout = function() {
-
-        };
-
-        $scope.gotodate = function(date) {
-            $scope.queryDate = date;
-            updateAll();
-        };
-
-        $scope.previous = function() {
-            var d1 = previousBlockDate();
-            $scope.queryDate = tb.utils.jsDate2py(d1);
-            updateAll();
-        };
-
-        $scope.next = function() {
-            $scope.queryDate = tb.utils.jsDate2py(nextBlockDate());
-            updateAll();
-        };
-
-        updateAll();
+//.config()
+  .controller('BlockSchedule', function($scope, $http) {
+    $scope.dates = []
+    $scope.guys = []
+    $scope.gals = []
+    $scope.subs = []
+    $scope.queryDate = null
+    $scope.initialized = false
+    
+    var updateInitialized = function() {
+      if ($scope.dates.length > 0
+        && $scope.guys.length > 0
+        && $scope.gals.length > 0) {
+        $scope.initialized = true
+      } else {
+        $scope.initialized = false
+      }
+      
+      $scope.isLastDate = isLastBlockDate()
+      $scope.isFirstDate = isFirstBlockDate()
+    }
+    
+    $http.get('/api/blockdates').success(function(data) {
+      $scope.dates = data
+      $scope.firstDate = tb.utils.pyDate2js(data[0].date)
+      $scope.lastDate = tb.utils.pyDate2js(data[data.length - 1].date)
+      $scope.blocksched = {}
+      _.each(data, function(d) {
+        var bdate = tb.utils.pyDate2js(d.date)
+        var ho = d.holdout
+        $scope.blocksched[bdate] = ho
+      })
+      updateInitialized()
     })
-    .controller('LatestBuzz', function($scope,$http) {
-        $http.get('/api/buzz/').success(function(data) {
-            $scope.buzzitems = data;
-        })
+    
+    var updateAll = function() {
+      $scope.initialized = false
+      
+      $http({
+        'url': '/api/blockplayers/date}/',
+        'method': 'GET',
+        'params': {'date': $scope.queryDate}
+      }).success(function(data) {
+        $scope.currdate = tb.utils.pyDate2js(data.date).toLocaleDateString()
+        $scope.guys = data.guys
+        $scope.gals = data.gals
+        updateInitialized()
+      })
+      
+      $http({
+        'url': '/api/subs/',
+        'method': 'GET',
+        'params': {'date': $scope.queryDate}
+      }).success(function(data) {
+        $scope.subs = data
+        updateInitialized()
+      })
+      
+      $http({
+        'url': '/api/blockschedule/',
+        'method': 'GET',
+        'params': {'date': $scope.queryDate}
+      }).success(function(data) {
+        $scope.slots = data
+        updateInitialized()
+      })
+    }
+    
+    var isLastBlockDate = function() {
+      if ($scope.lastDate && $scope.currdate) {
+        return $scope.currdate == $scope.lastDate.toLocaleDateString()
+      }
+      
+      return false
+    }
+    
+    var isFirstBlockDate = function() {
+      if ($scope.firstDate && $scope.currdate) {
+        return $scope.currdate == $scope.firstDate.toLocaleDateString()
+      }
+      return false
+    }
+    
+    var previousBlockDate = function() {
+      var d1 = new Date($scope.currdate)
+      if (d1.toDateString() == $scope.firstDate.toDateString()) {
+        return d1
+      }
+      var d2 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate() - 7)
+      while (d2.toDateString() != $scope.lastDate.toDateString() && $scope.blocksched[d2] == 1) {
+        d2 = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate() - 7)
+      }
+      
+      return d2
+    }
+    
+    var nextBlockDate = function() {
+      var d1 = new Date($scope.currdate)
+      if (d1.toDateString() == $scope.lastDate.toDateString()) {
+        return d1
+      }
+      var d2 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate() + 7)
+      while (d2.toDateString() != $scope.lastDate.toDateString() && $scope.blocksched[d2] == 1) {
+        d2 = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate() + 7)
+      }
+      
+      return d2
+    }
+    
+    /**
+     * schedulePlayer
+     *
+     * Update the schedule for the current block session
+     */
+    $scope.schedulePlayer = function() {
+    
+    }
+    
+    $scope.isHoldout = function() {
+    
+    }
+    
+    $scope.gotodate = function(date) {
+      $scope.queryDate = date
+      updateAll()
+    }
+    
+    $scope.previous = function() {
+      var d1 = previousBlockDate()
+      $scope.queryDate = tb.utils.jsDate2py(d1)
+      updateAll()
+    }
+    
+    $scope.next = function() {
+      $scope.queryDate = tb.utils.jsDate2py(nextBlockDate())
+      updateAll()
+    }
+    
+    updateAll()
+  })
+  .controller('LatestBuzz', function($scope, $http) {
+    $http.get('/api/buzz/').success(function(data) {
+      $scope.buzzitems = data
     })
-    .controller('BlockPlayers', function($scope,$http) {
-        $scope.couples = [];
-
-        $http.get('/api/blockplayers').success(function(data) {
-            $scope.couples = data;
-        });
-
+  })
+  .controller('BlockPlayers', function($scope, $http) {
+    $scope.couples = []
+    
+    $http.get('/api/blockplayers').success(function(data) {
+      $scope.couples = data
     })
-    .controller('Availability', function($scope,$http) {
-        $scope.dates = [];
-        $scope.players = [];
-        $scope.initialized = false;
-
-        var updateInitialized = function() {
-            if ($scope.dates.length > 0 && $scope.players.length > 0) {
-                $scope.initialized = true;
-            } else {
-                $scope.initialized = false;
-            }
-        };
-
-        $http.get('/api/blockdates').success(function(data) {
-            $scope.dates= data;
-            updateInitialized();
-        });
-        $http.get('/api/availability').success(function(data) {
-            $scope.players = data;
-            updateInitialized();
-        });
-
-        $scope.isHoldout = function() {
-
-        };
-
-        $scope.updateAvail = function(p,idx) {
-            console.log('Updating for player ' + p.id + " index " + idx + " is avail?" + p.isavail[idx]);
-            $http.put('/api/availability/',{
-                'id' : p.id,
-                'mtgidx' : idx,
-                'isavail' : p.isavail[idx]
-            });
-        };
-
-        $scope.computeNeeded = function() {
-            //$scope.funding.needed = $scope.funding.startingEstimate * 10;
-        };
-
-        $scope.requestFunding = function() {
-            window.alert("Sorry");
-        };
-
-        $scope.reset = function() {
-            //$scope.funding.startingEstimate = 0;
-        };
-
-        //$scope.$watch('funding.startingEstimate',$scope.computeNeeded);
-    });
+    
+  })
+  .controller('Availability', function($scope, $http) {
+    $scope.dates = []
+    $scope.players = []
+    $scope.initialized = false
+    
+    var updateInitialized = function() {
+      if ($scope.dates.length > 0 && $scope.players.length > 0) {
+        $scope.initialized = true
+      } else {
+        $scope.initialized = false
+      }
+    }
+    
+    $http.get('/api/blockdates').success(function(data) {
+      $scope.dates = data
+      updateInitialized()
+    })
+    $http.get('/api/availability').success(function(data) {
+      console.log('calling availability from module')
+      $scope.players = data
+      updateInitialized()
+    })
+    
+    $scope.isHoldout = function() {
+    
+    }
+    
+    $scope.updateAvail = function(p, idx) {
+      console.log('Updating for player ' + p.id + " index " + idx + " is avail?" + p.isavail[idx])
+      $http.put('/api/availability/', {
+        'id': p.id,
+        'mtgidx': idx,
+        'isavail': p.isavail[idx]
+      })
+    }
+    
+    $scope.computeNeeded = function() {
+      //$scope.funding.needed = $scope.funding.startingEstimate * 10;
+    }
+    
+    $scope.requestFunding = function() {
+      window.alert("Sorry")
+    }
+    
+    $scope.reset = function() {
+      //$scope.funding.startingEstimate = 0;
+    }
+    
+    //$scope.$watch('funding.startingEstimate',$scope.computeNeeded);
+  })
 
 
 
--- tennisblock/webapp/static/js/angular/services/factories.js
+++ tennisblock/webapp/static/js/angular/services/factories.js
@@ -1,56 +1,56 @@
-
-tennisblockapp.factory('BlockDates',function ($resource) {
-    return $resource('/api/blockdates/', {}, {
-        query: {method: 'GET', isArray: true}
-    })
-}).factory('BlockPlayers',function ($resource) {
-    return $resource('/api/blockplayers/:date/', {date: '@date'}, {
-        get: {method: 'GET', params: {ver: '@ver'},isArray: false}
-    })
-}).factory('BlockSubs',function ($resource) {
-    return $resource('/api/subs/:date/', {date: '@date'}, {
-        get: {method: 'GET', params: {ver: '@ver'}, isArray: false}
-    })
-
-}).factory('PickTeams',function ($resource) {
-    return $resource('/api/pickteams/:date/', {}, {
-        get: {method: 'GET', params: {date: '@date'}, isArray: false},
-        save:  {method: 'POST', params: {date: '@date'}, isArray: false}
-    })
-}).factory('PlaySheet',function ($resource) {
-    return $resource('/api/blocksheet2/:date', {}, {
-        get: {method: 'GET', params: {date: '@date'}, isArray: true}
-    })
-}).factory('TeamSchedule',function ($resource) {
-    return $resource('/api/matchdata/:date/', {}, {
-        get: {method: 'GET', params: {date: '@date', ver: '@ver'}, isArray: false}
-    })
-}).factory('Availability',function ($resource) {
-    return $resource('/api/availability/', {}, {
-        query: {method: 'GET', isArray: true}
-    })
-}).factory('BlockBuzz',function ($resource) {
-    return $resource('/api/buzz/', {}, {
-        query: {method: 'GET', isArray: true}
-    })
-}).factory('BlockSchedule', function ($resource) {
-    return $resource('/api/blockschedule/:date/', {}, {
-        get: {method: 'GET', isArray: true},
-        remove: { method: 'DELETE', params: {date: '@date'}  },
-        save: {method: 'POST', params: {date: '@date'}, isArray: false}
-    })
-}).factory('Members', function ($resource) {
-    return $resource('/api/members/:id/', {}, {
-        get: {method: 'GET', params: {id: '@id'}},
-        save: {method: 'POST', isArray: false},
-        update: {method: 'POST', params: {id: '@id'},isArray: false},
-        insert: { method: 'PUT', isArray: false},
-        remove: { method: 'DELETE'}
-    })
+tennisblockapp.factory('BlockDates', function($resource) {
+  return $resource('/api/blockdates/', {}, {
+    query: {method: 'GET', isArray: true}
+  })
+}).factory('BlockPlayers', function($resource) {
+  return $resource('/api/blockplayers/:date/', {date: '@date'}, {
+    get: {method: 'GET', params: {ver: '@ver'}, isArray: false}
+  })
+}).factory('BlockSubs', function($resource) {
+  return $resource('/api/subs/:date/', {date: '@date'}, {
+    get: {method: 'GET', params: {ver: '@ver'}, isArray: false}
+  })
+  
+}).factory('PickTeams', function($resource) {
+  return $resource('/api/pickteams/:date/', {}, {
+    get: {method: 'GET', params: {date: '@date'}, isArray: false},
+    save: {method: 'POST', params: {date: '@date'}, isArray: false}
+  })
+}).factory('PlaySheet', function($resource) {
+  return $resource('/api/blocksheet2/:date', {}, {
+    get: {method: 'GET', params: {date: '@date'}, isArray: true}
+  })
+}).factory('TeamSchedule', function($resource) {
+  return $resource('/api/matchdata/:date/', {}, {
+    get: {method: 'GET', params: {date: '@date', ver: '@ver'}, isArray: false}
+  })
+}).factory('Availability', function($resource) {
+  console.log('calling availability from factory')
+  return $resource('/api/availability/', {}, {
+    query: {method: 'GET', isArray: true}
+  })
+}).factory('BlockBuzz', function($resource) {
+  return $resource('/api/buzz/', {}, {
+    query: {method: 'GET', isArray: true}
+  })
+}).factory('BlockSchedule', function($resource) {
+  return $resource('/api/blockschedule/:date/', {}, {
+    get: {method: 'GET', isArray: true},
+    remove: {method: 'DELETE', params: {date: '@date'}},
+    save: {method: 'POST', params: {date: '@date'}, isArray: false}
+  })
+}).factory('Members', function($resource) {
+  return $resource('/api/members/:id/', {}, {
+    get: {method: 'GET', params: {id: '@id'}},
+    save: {method: 'POST', isArray: false},
+    update: {method: 'POST', params: {id: '@id'}, isArray: false},
+    insert: {method: 'PUT', isArray: false},
+    remove: {method: 'DELETE'}
+  })
 }).factory("SendSchedule", function($resource) {
-    return $resource('/api/schedule/notify/:date/',{date: '@date'}, {
-        get: {method: 'GET', params: {}},
-        update: {method: 'POST', params: {},isArray: false}
-    })
-});
+  return $resource('/api/schedule/notify/:date/', {date: '@date'}, {
+    get: {method: 'GET', params: {}},
+    update: {method: 'POST', params: {}, isArray: false}
+  })
+})
 
