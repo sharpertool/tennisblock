e3a124283dd0ddd84203ad9e034bef6dfc9f5cb4
ed@sharpertool.com <ed@sharpertool.com>
2018-12-07 15:47:21 -0700

    Split date before parsing to use local timezone

--- tennisblock_client/src/components/ui/Tiles/index.js
+++ tennisblock_client/src/components/ui/Tiles/index.js
@@ -1,19 +1,25 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 import styles from './styles.local.scss'
 
-const tiles = ({ dates }) => {
-  return dates && dates.map(({ date }, index) =>
-      <Link to={`/schedule/${date}`} key={index} className={`${styles.tile} col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 pt-2`}>
-          <div className={styles.tileMonth}>
-            {new Date(date).toLocaleString('en-us', { month: "long" })}
-          </div>
-          <div className={styles.tileDay}>
-            {new Date(date).toLocaleString('en-us', { day: "numeric" })}
-          </div>
-      </Link>)
+const tiles = ({dates}) => {
+  return dates && dates.map(({date}, index) => {
+    const localdate = new Date(date.split('-'))
+    
+    return (
+      <Link to={`/schedule/${date}`} key={index}
+            className={`${styles.tile} col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 pt-2`}>
+        <div className={styles.tileMonth}>
+          {localdate.getMonth()+1}
+        </div>
+        <div className={styles.tileDay}>
+          {localdate.getDate()}
+        </div>
+      </Link>
+    )
+  })
 }
 
 tiles.propTypes = {
